c9d8b7aa65831e1cdd4f0d5dc6b891b28e62b48a
Alina Shcherbinina <alinkash@gmail.com>
2022-03-24 02:00:38 +0800

    init plus bottom navigation menu with simple fragments

---
+++ app/src/main/java/com/example/myastronomy/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.myastronomy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
+
+
+        navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_map:
+                        selectedFragment = new MapFragment();
+                        break;
+                    case R.id.nav_news:
+                        selectedFragment = new NewsFragment();
+                        break;
+                    case R.id.nav_book:
+                        selectedFragment = new BooksFragment();
+                        break;
+                    case R.id.nav_test:
+                        selectedFragment = new TestsFragment();
+                        break;
+                    case R.id.nav_acc:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        });
+
+
+    }
+
+
+
+}
